f03a55f14a54dc0da60ff0747a34da9793e02a21
Dana <95848273+DanaBoo@users.noreply.github.com>
2023-12-11 09:25:33 +0000

    Update 6 files

--- src/AlgorithmChart.js
+++ src/AlgorithmChart.js
@@ -1,13 +1,18 @@
 import React from "react";
+import "./HomePage.css"; // Підключення стилів сторінки
 import ReactFlow, { Background, Controls, MiniMap } from "react-flow-renderer";
 
+// Встановлення елементів алгоритму
 const nodes = [
   {
     id: "1",
     type: "default",
     data: { label: "Start" },
     position: { x: 100, y: 100 },
+    hidden: false,
     style: {
+      width: "10px",
+      height: "10px",
       background: "#e3002b",
       border: "1px solid #333",
       borderRadius: "8px",
@@ -28,6 +33,7 @@ const nodes = [
   },
 ];
 
+// Встіновлення зв'язків між елементами
 const edges = [
   {
     id: "e1-2",
@@ -36,9 +42,9 @@ const edges = [
     animated: true,
     style: { stroke: "#000", strokeWidth: "2px" },
   },
-  // Define connections between nodes
 ];
 
+// Функціональний компонент для відображення діаграми алгоритму
 const AlgorithmChart = () => {
   return (
     <div style={{ height: "900px", width: "100%", margin: "0 auto" }}>
@@ -51,16 +57,79 @@ const AlgorithmChart = () => {
   );
 };
 
-{
-  /* const AlgorithmChart = () => {
-  return (
-    <div style={{ height: "500px", width: "80%", margin: "0 auto" }}>
-      <ReactFlow elements={nodes.concat(edges)}>
-        <Background />
-      </ReactFlow>
-    </div>
-  );
-}; */
-}
-
 export default AlgorithmChart;
+
+{
+  /*}
+import React, { useCallback } from "react";
+import ReactFlow, {
+  addEdge,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+} from "reactflow";
+
+import {
+  nodes as initialNodes,
+  edges as initialEdges,
+} from "./initial-elements";
+import CustomNode from "./CustomNode";
+
+import "reactflow/dist/style.css";
+import "./overview.css";
+
+const nodeTypes = {
+  custom: CustomNode,
+};
+
+const minimapStyle = {
+  height: 120,
+};
+
+const onInit = (reactFlowInstance) =>
+  console.log("flow loaded:", reactFlowInstance);
+
+const OverviewFlow = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [],
+  );
+
+  // we are using a bit of a shortcut here to adjust the edge type
+  // this could also be done with a custom edge for example
+  const edgesWithUpdatedTypes = edges.map((edge) => {
+    if (edge.sourceHandle) {
+      const edgeType = nodes.find((node) => node.type === "custom").data
+        .selects[edge.sourceHandle];
+      edge.type = edgeType;
+    }
+
+    return edge;
+  });
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edgesWithUpdatedTypes}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      onInit={onInit}
+      fitView
+      attributionPosition="top-right"
+      nodeTypes={nodeTypes}
+    >
+      <MiniMap style={minimapStyle} zoomable pannable />
+      <Controls />
+      <Background color="#aaa" gap={16} />
+    </ReactFlow>
+  );
+};
+
+export default OverviewFlow;
+*/
+}
--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./HomePage";
-import LecturePage from "./LecturePage";
-import AlgorithmPage from "./AlgorithmPage";
-import ResourcesPage from "./ResourcesPage";
-import ContactPage from "./ContactPage";
+import HomePage from "./HomePage"; // Імпорт компоненти головної сторінки
+import LecturePage from "./LecturePage"; // Імпорт компоненти сторінки порядку ДМД
+import AlgorithmPage from "./AlgorithmPage"; // Імпорт компоненти сторінки алгоритму
+import ResourcesPage from "./ResourcesPage"; // Імпорт компоненти сторінки ресурсів
+import ContactPage from "./ContactPage"; // Імпорт компоненти сторінки контактів
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/lecture" element={<LecturePage />} />
-        <Route path="/algorithm" element={<AlgorithmPage />} />
-        <Route path="/resources" element={<ResourcesPage />} />
-        <Route path="/contact" element={<ContactPage />} />
+        {/* Маршрутизація до різних сторінок */}
+        <Route path="/" element={<HomePage />} />{" "}
+        {/* Маршрут до головної сторінки */}
+        <Route path="/lecture" element={<LecturePage />} />{" "}
+        {/* Маршрут до сторінки порядку ДМД */}
+        <Route path="/algorithm" element={<AlgorithmPage />} />{" "}
+        {/* Маршрут до сторінки алгоритму */}
+        <Route path="/resources" element={<ResourcesPage />} />{" "}
+        {/* Маршрут до сторінки ресурсів */}
+        <Route path="/contact" element={<ContactPage />} />{" "}
+        {/* Маршрут до сторінки контактів */}
       </Routes>
     </Router>
   );
