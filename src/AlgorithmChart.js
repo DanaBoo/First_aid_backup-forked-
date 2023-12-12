f758b39dcbb9baa2838347209a41b8b94a8a7a77
Dana <95848273+DanaBoo@users.noreply.github.com>
2023-12-12 21:33:42 +0000

    fix: Update positions and data in AlgorithmChart.js
    Fixes incorrect positions and adds new data to AlgorithmChart component.

--- src/AlgorithmChart.js
+++ src/AlgorithmChart.js
@@ -8,19 +8,19 @@ const initialNodes = [
     id: "1",
     type: "default",
     data: { label: "Виявлення постраждалого" },
-    position: { x: 805, y: 10 },
+    position: { x: 800, y: 10 },
   },
   {
     id: "2",
     type: "default",
     data: { label: "Постраждалий з термічним опіком" },
-    position: { x: 805, y: 25 },
+    position: { x: 800, y: 25 },
   },
   {
     id: "3",
     type: "default",
     data: { label: "Місце безпечне?" },
-    position: { x: 805, y: 40 },
+    position: { x: 800, y: 40 },
   },
   {
     id: "4",
@@ -92,25 +92,31 @@ const initialNodes = [
     id: "15",
     type: "default",
     data: { label: "Є можливість усунути небезпеку?" },
-    position: { x: 300, y: 300 },
+    position: { x: 850, y: 60 },
   },
   {
     id: "16",
     type: "default",
     data: { label: "Усунути небезпеку" },
-    position: { x: 100, y: 100 },
+    position: { x: 825, y: 90 },
   },
   {
     id: "17",
     type: "default",
     data: { label: "Залишатися у безпеці" },
-    position: { x: 200, y: 200 },
+    position: { x: 875, y: 90 },
   },
   {
     id: "18",
     type: "default",
+    data: { label: "Викликати ЕМД" },
+    position: { x: 875, y: 105 },
+  },
+  {
+    id: "19",
+    type: "default",
     data: { label: "Дотримуватися вказівок диспетчера" },
-    position: { x: 300, y: 300 },
+    position: { x: 875, y: 120 },
   },
 ];
 
@@ -118,32 +124,36 @@ const initialNodes = [
 const initialEdges = [
   { id: "e1-2", source: "1", target: "2" },
   { id: "e2-3", source: "2", target: "3" },
-  { id: "e3-4", source: "1", target: "2" },
-  { id: "e4-5", source: "2", target: "3" },
-  { id: "e5-6", source: "1", target: "2" },
-  { id: "e2-3", source: "2", target: "3" },
-  { id: "e1-2", source: "1", target: "2" },
-  { id: "e2-3", source: "2", target: "3" },
-  { id: "e1-2", source: "1", target: "2" },
-  { id: "e2-3", source: "2", target: "3" },
-  { id: "e1-2", source: "1", target: "2" },
-  { id: "e2-3", source: "2", target: "3" },
+  { id: "e3-4", source: "3", target: "4" },
+  { id: "e4-5", source: "4", target: "5" },
+  { id: "e5-6", source: "5", target: "6" },
+  { id: "e6-7", source: "6", target: "7" },
+  { id: "e7-8", source: "7", target: "8" },
+  { id: "e8-9", source: "8", target: "9" },
+  { id: "e9-10", source: "9", target: "10" },
+  { id: "e10-11", source: "10", target: "11" },
+  { id: "e11-14", source: "11", target: "14" },
+  { id: "e10-12", source: "10", target: "12" },
+  { id: "e12-13", source: "12", target: "13" },
+  { id: "e13-14", source: "13", target: "14" },
+  { id: "e10-12", source: "10", target: "12" },
+  { id: "e12-13", source: "12", target: "13" },
 ];
 
 const AlgorithmChart = () => {
   // Використання React Hooks для стану вузлів та з'єднань
-  console.log("load element");
+  /*console.log("load element");
   const [nodes] = React.useState(initialNodes);
   const [edges] = React.useState(initialEdges);
 
-  console.log("load element");
+  console.log("load element");*/
 
   try {
     return (
       // Контейнер для ReactFlow компоненту
       <div style={{ height: "900px", width: "100%", margin: "0 auto" }}>
         {/* ReactFlow компонент */}
-        <ReactFlow elements={nodes.concat(edges)}>
+        <ReactFlow nodes={initialNodes} edges={initialEdges}>
           {/* Додаткові елементи для ReactFlow: Background, Controls, MiniMap */}
           <Background />
           <Controls />
